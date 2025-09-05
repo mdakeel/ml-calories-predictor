23d6929462aae746139b86b23c9b153d4aaf4272
Md Akeel <aakiltayyab@gmail.com>
2025-09-05 13:17:31 +0530

    Apply best experiment with test_size=0.2

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import yaml
 
 df = pd.read_csv("data/raw/row.csv")
 X = df.drop(columns=["calories(gm)"])
 y = df["calories(gm)"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+with open("params.yaml") as f:
+    params = yaml.safe_load(f)
+    
+test_size = params["train"]["test_size"]
+random_state = params["train"]["random_state"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 X_train.to_csv("data/X_train.csv", index=False)
 X_test.to_csv("data/X_test.csv", index=False)
